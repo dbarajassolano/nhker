1048e75fb08dd0adbe9ef498abd4bd393d19f784
David A. Barajas-Solano <David.Barajas-Solano@pnnl.gov>
2023-05-28 18:36:05 -0700

    Revert "Show "loading""
    This reverts commit 4a607641f57bc172703b326837ad97a78f0d6828.

--- nhker.py
+++ nhker.py
@@ -1,11 +1,9 @@
 import webbrowser
-from threading import Timer, Thread
+from threading import Timer
 from flask import Flask, render_template
-from turbo_flask import Turbo
 from parse import NewsParser
 
 app = Flask(__name__)
-turbo = Turbo(app)
 
 np = NewsParser()
 
@@ -13,17 +11,11 @@ np = NewsParser()
 def main():
     return render_template('index.html', articles=np.articles)
 
-def update_article(article_id: int) -> None:
-    with app.app_context():
-        title, body = np.parse_article_threaded(article_id)
-        turbo.push(turbo.replace(render_template('article.html', title=title, body=body), 'article_content'))
-
 @app.route('/<int:article_id>')
 def show_article(article_id):
-    tparse = Thread(target=update_article, args=[article_id])
-    tparse.start()
-    return render_template('loading.html')
-
+    title, body = np.parse_article(article_id)
+    return render_template('article.html', title=title, body=body)
+    
 def open_browser():
       webbrowser.open_new("http://127.0.0.1:5000")
 
